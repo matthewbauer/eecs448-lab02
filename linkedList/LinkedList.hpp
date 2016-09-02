1fe258eb3cbf065e95ebc48f0e7668157ffdd36b
Matthew Bauer <mjbauer95@gmail.com>
2016-09-02 14:57:35 -0500

    Implement LinkedList<T>::size()

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,10 +29,8 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
-	return(0);
+	Node<T>* temp = m_front;
+	return(m_size);
 }
 
 template <typename T>
